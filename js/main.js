1d462702af771951e5a7142339f61fd200fca70c
工藤亜美 <amxxkey@gmail.com>
2021-06-07 01:11:57 +0900

    change main.js

--- js/main.js
+++ js/main.js
@@ -42,23 +42,23 @@ var app2 = new Vue({
             ['カレーうどん', '16-152-913', 'カレー', 'カレールー',　'うどん'],
             ['素麺・冷麦', 'そうめん', '素麺',　'めんつゆ',　'麺ゆつ'],
             ['あさり味噌汁', '17-159-1355', 'あさり', '味噌', 'ねぎ'],
-            ['しじみ味噌汁', '', 'しじみ',　'味噌', 'ねぎ'],
+            ['しじみ味噌汁', '17-159-1356', 'しじみ',　'味噌', 'ねぎ'],
             ['大根の味噌汁', '大根', '味噌'],
             ['豆腐の味噌汁', '豆腐', '絹豆腐', '木綿豆腐', '味噌'],
             ['ビビンバ', 'コチュジャン', 'もやし', 'にんじん', 'ひき肉', '牛挽肉', '豚挽肉', '合挽肉'],
-            ['キーマカレー', 'トマト', 'ひき肉', '牛挽肉', '豚挽肉', '合挽肉'],
-            ['ポテトサラダ', 'じゃがいも', 'マヨネーズ'],
-            ['きんぴらごぼう', 'にんじん', 'ごぼう'],
+            ['キーマカレー', '30-307-1158', 'トマト', 'ひき肉', '牛挽肉', '豚挽肉', '合挽肉'],
+            ['ポテトサラダ', '18-415', 'じゃがいも', 'マヨネーズ', '玉ねぎ'],
+            ['きんぴらごぼう', '30-311-1192', 'にんじん', 'ごぼう'],
             ['豆乳鍋', '23-397', '豆乳', '豆腐', '白菜'],
             ['キムチ鍋', '23-395', 'キムチ', '白菜', '豚バラ肉'],
-            ['ピーマンの肉詰め', 'ピーマン', 'ひき肉', '牛挽肉', '豚挽肉', '合挽肉'],
-            ['マーボー豆腐（麻婆豆腐）', '豆腐', '絹豆腐', '木綿豆腐', 'ひき肉', '牛挽肉', '豚挽肉', '合挽肉'],
+            ['ピーマンの肉詰め', '31-321', 'ピーマン', 'ひき肉', '牛挽肉', '豚挽肉', '合挽肉'],
+            ['マーボー豆腐（麻婆豆腐）', '41-534', '豆腐', '絹豆腐', '木綿豆腐', 'ひき肉', '牛挽肉', '豚挽肉', '合挽肉'],
             ['オムライス', '14-121', 'たまご', '卵', 'ケチャップ', '玉ねぎ', 'たまねぎ'], 
-            ['チキンライス', '鶏モモ肉', '玉ねぎ', 'たまねぎ', 'ケチャップ'],
-            ['エビピラフ', 'えび', '海老', 'たまねぎ', '玉ねぎ', 'バター'],
-            ['豚丼',  '豚薄切り肉',　'たまねぎ',　'玉ねぎ'],
-            ['中華丼',  'きくらげ',　'うずらたまご'],
-            ['カツ丼', 'たまご', '卵', '豚ロース'],
+            ['チキンライス', '14-122', '鶏モモ肉', '玉ねぎ', 'たまねぎ', 'ケチャップ'],
+            ['エビピラフ', '14-127', 'えび', '海老', 'たまねぎ', '玉ねぎ', 'バター'],
+            ['豚丼', '14-130-544',  '豚薄切り肉',　'たまねぎ',　'玉ねぎ'],
+            ['中華丼', '14-130-546', '豚肉', '豚バラ', '豚薄切り肉', '白菜', 'うずらたまご'],
+            ['カツ丼', '14-130-542', 'たまご', '卵', '豚ロース', '豚ロース肉', '豚ヒレ肉'],
             ['フレンチトースト', '22-433', '食パン', '牛乳']
           ],
     abc: [],
@@ -100,24 +100,9 @@ var app2 = new Vue({
   createRecipe: function(index){
     axios.get('https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1081707383800108253&categoryId=' + index)
     .then(function(response){
-<<<<<<< HEAD
       for(var x=0; x<4; x++){
       this.recipes.push({title: response.data.result[x].recipeTitle, images: response.data.result[x].foodImageUrl, url: response.data.result[x].recipeUrl})
     }
-=======
-      this.recipes.push({images: response.data.result[0].foodImageUrl})
-      this.recipes.push({url: response.data.result[0].recipeUrl})
-      this.recipes.push({title: response.data.result[0].recipeTitle})
-      this.recipes.push({images: response.data.result[1].foodImageUrl})
-      this.recipes.push({url: response.data.result[1].recipeUrl})
-      this.recipes.push({title: response.data.result[1].recipeTitle})
-      this.recipes.push({images: response.data.result[2].foodImageUrl})
-      this.recipes.push({url:　response.data.result[2].recipeUrl})
-      this.recipes.push({title: response.data.result[2].recipeTitle})
-      this.recipes.push({images: response.data.result[3].foodImageUrl})
-      this.recipes.push({url: response.data.result[3].recipeUrl})
-      this.recipes.push({title: response.data.result[3].recipeTitle})
->>>>>>> origin/master
       // console.log(response)
     }.bind(this))
     .catch(function(error){
@@ -127,34 +112,3 @@ var app2 = new Vue({
 }
 })
 
-<<<<<<< HEAD
-=======
-
-
-// var app3 = new Vue({
-//     el: '#weather',
-//     data: {
-//       city: null,
-//       temp: null,
-//       condition: {
-//       main: null
-//       }
-//     },
-//     mounted: function(){
-//       axios.get('https://api.openweathermap.org/data/2.5/weather?q=Tokyo,jp&units=metric&appid=f3b18c03f19e675e32c44b6b0af50e64')
-//       .then(function(response){
-//         this.city = response.data.name
-//         this.temp = response.data.main.temp
-//         this.condition = response.data.weather[0]
-//       }.bind(this))
-//       .catch(function(error){
-//         console.log(error)
-//       })
-//     },
-//     filters: {
-//       roundUp(value){
-//         return Math.ceil(value)
-//       }
-//     }
-// })
->>>>>>> origin/master
