c8099139aea861fcfeacd9573a248516c8017423
工藤亜美 <amxxkey@gmail.com>
2021-06-29 16:48:01 +0900

    add alert

--- js/main.js
+++ js/main.js
@@ -78,6 +78,10 @@ var app2 = new Vue({
   },
   methods: {
     fnc: function() {
+      if(app1.foods == ''){
+        alert('※左画面から材料を登録してください')
+        return
+      }
         this.show = false
         this.show_after = true
         for(var a=0; a < this.arry.length; a++){
